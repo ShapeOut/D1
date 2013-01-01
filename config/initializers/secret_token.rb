719233c0daaae77116f6d81840d6e09577187b25
ShapeOut <shapeout1@gmail.com>
2012-12-31 22:25:59 -0800

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '20b5248c6742e08f3dcab74fccb4934503258ac855bf40220d0a57679066542d8e3761c9a8928d134a0c3114558112134f0f2b5b74afa754f66e255a553b3491'
